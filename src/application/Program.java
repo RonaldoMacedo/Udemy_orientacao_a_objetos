98043331d983993ba2821f722a28126f54725730
RonaldoMacedo <ronni29_100@hotmail.com>
2024-01-12 11:25:12 -0300

    Aula 063

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,40 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com os lados do triângulo x: ");
+		double xA = sc.nextDouble();
+		double xB = sc.nextDouble();
+		double xC = sc.nextDouble();
+		System.out.println("Entre com os lados do triângulo y: ");
+		double yA = sc.nextDouble();
+		double yB = sc.nextDouble();
+		double yC = sc.nextDouble();
+		
+		double p = (xA + xB + xC) / 2.0;
+		double areaX = Math.sqrt(p *(p - xA) * (p - xB) * (p - xC));
+		
+		p = (yA + yB + yC) / 2.0;
+		double areaY = Math.sqrt(p *(p - yA) * (p - yB) * (p - yC));
+		
+		System.out.printf("Área do triângulo x: %.4f%n", areaX);
+		System.out.printf("Área do triângulo y: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Maior área: X");
+		}else {
+			System.out.println("Maior área: Y");
+		}
+		
+		sc.close();
 	}
 
 }
