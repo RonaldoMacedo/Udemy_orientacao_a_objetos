de3cab29c4c04dc014e381e589d0b4d801d65250
RonaldoMacedo <ronni29_100@hotmail.com>
2024-01-17 12:43:45 -0300

    Aula 065

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,22 +12,20 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Entre com os lados do triângulo x: ");
 		Triangulo x = new Triangulo();
+		Triangulo y = new Triangulo();
+		
+		System.out.println("Entre com os lados do triângulo x: ");
 		x.a = sc.nextDouble();
 		x.b = sc.nextDouble();
 		x.c = sc.nextDouble();
 		System.out.println("Entre com os lados do triângulo y: ");
-		Triangulo y = new Triangulo();
 		y.a = sc.nextDouble();
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p *(p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p *(p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Área do triângulo x: %.4f%n", areaX);
 		System.out.printf("Área do triângulo y: %.4f%n", areaY);
--- src/entities/Triangulo.java
+++ src/entities/Triangulo.java
@@ -3,5 +3,10 @@ package entities;
 public class Triangulo {
 	
 	public double a, b, c;
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 
 }
