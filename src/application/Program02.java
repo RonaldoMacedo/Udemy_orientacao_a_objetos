1af509cd4249d391a956b40ff34a3db3fccbb6a0
RonaldoMacedo <ronni29_100@hotmail.com>
2024-01-17 15:20:10 -0300

    Aula 068

--- src/application/Program02.java
+++ src/application/Program02.java
@@ -23,7 +23,23 @@ public class Program02 {
 		product.quantity = sc.nextInt();
 		
 		System.out.println();
-		System.out.println(product);
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("updated data: " + product);
 		
 		sc.close();
 
