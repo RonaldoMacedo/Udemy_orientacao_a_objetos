1ce0299353cdf572120dec0d8ac2b07a88d397a2
RonaldoMacedo <ronni29_100@hotmail.com>
2024-01-18 10:04:40 -0300

    Aula 070 - versão 2

--- src/application/Program06.java
+++ src/application/Program06.java
@@ -3,36 +3,30 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.Calculator;
+
 public class Program06 {
-	
-	public static final double PI = 3.14159; //membro estático //palavra final valor não mudará
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Enter radius:");
+		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = circumference(radius);
+		Calculator calc = new Calculator();
 		
-		double v = volume(radius);
+		double c = calc.circumference(radius);
+		
+		double v = calc.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f", PI);
+		System.out.printf("PI value: %.2f", calc.PI);
 		
 		sc.close();
 
-	}
-	
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 
 }
